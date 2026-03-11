051f890a3f87fdafd843fd68bce404d934a2f559
saipriyagudipati13-sudo <saipriyagudipati13@gmail.com>
2026-03-11 12:26:11 +0530

    updated layout footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Cart from "./components/Cart";
+import Orders from "./components/Orders";
 function App() {
   return (
     <div>
       <Header />
       <Content />
+      <Login/>
+      <Register/>
+      <Cart/>
+      <Orders/>
       <Footer />
     </div>
   );
